96d2f45a9fa6cc2613b106f1cb0ada32e9fe2877
Anderson Matos <ams.bns2021@gmail.com>
2021-12-22 20:58:16 -0300

    criação do modulo produdoro e componentes focos

---
+++ src/app/PP-2-FE-usuario-muda-status/produdoro/focos/focos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FocosComponent } from './focos.component';
+
+describe('FocosComponent', () => {
+  let component: FocosComponent;
+  let fixture: ComponentFixture<FocosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FocosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FocosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/PP-2-FE-usuario-muda-status/produdoro/focos/focos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-focos',
+  templateUrl: './focos.component.html',
+  styleUrls: ['./focos.component.css']
+})
+export class FocosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
