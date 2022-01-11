7565c9531f2fa2653500b57100a582a69754231c
Romano dos Anjos <dosanjosromano@gmail.com>
2022-01-11 12:07:39 -0300

    Navegação entre componentes utilizando router link

--- src/app/produdoro/produdoro.module.ts
+++ src/app/produdoro/produdoro.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProdudoroRoutingModule } from './produdoro-routing.module';
 import { PausaCurtaComponent } from './components/pausa-curta/pausa-curta/pausa-curta.component';
 import { PausaLongaComponent } from './components/pausa-longa/pausa-longa/pausa-longa.component';
+import { FocoComponent } from './components/foco/foco.component';
 
 
 @NgModule({
   declarations: [
+    FocoComponent,
     PausaCurtaComponent,
     PausaLongaComponent
   ],
